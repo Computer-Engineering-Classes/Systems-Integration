1bff6f5b95da0664823f9bcf1ce6a86d94103735
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-03-16 16:19:25 +0000

    Web APIs

---
+++ WebApi/StudentsApiConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using StudentsApi;
+
+// REST API Client
+while (true)
+{
+    Console.WriteLine("Student API Menu:");
+    Console.WriteLine("\t1. Get all students");
+    Console.WriteLine("\t2. Get student by id");
+    Console.WriteLine("\t3. Add student");
+    Console.WriteLine("\t4. Update student");
+    Console.WriteLine("\t5. Delete student");
+    Console.WriteLine("\t6. Exit");
+    Console.Write("Enter your choice: ");
+    var choice = Console.ReadLine();
+    int id;
+    string? name, color;
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine(StudentApi.GetStudents());
+            break;
+        case "2":
+            Console.Write("Enter id: ");
+            if (int.TryParse(Console.ReadLine(), out id) == false) continue;
+            Console.WriteLine(StudentApi.GetStudent(id));
+            break;
+        case "3":
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter color: ");
+            color = Console.ReadLine();
+            var student = new Student
+            {
+                Name = name,
+                Color = color
+            };
+            Console.WriteLine(StudentApi.AddStudent(student));
+            break;
+        case "4":
+            Console.Write("Enter id: ");
+            if (int.TryParse(Console.ReadLine(), out id) == false) continue;
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter color: ");
+            color = Console.ReadLine();
+            student = new Student
+            {
+                Name = name,
+                Color = color
+            };
+            Console.WriteLine(StudentApi.UpdateStudent(id, student));
+            break;
+        case "5":
+            Console.Write("Enter id: ");
+            if (int.TryParse(Console.ReadLine(), out id) == false) continue;
+            Console.WriteLine(StudentApi.DeleteStudent(id));
+            break;
+        case "6":
+            return;
+        default:
+            Console.WriteLine("Invalid option. Try again.");
+            break;
+    }
+}
---
+++ WebApi/StudentsApiConsole/StudentApi.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+
+namespace StudentsApi;
+
+public static class StudentApi
+{
+    private static readonly HttpClient Client = new();
+    private const string ApiUrl = "http://cd9c-193-136-157-72.ngrok.io/api/values";
+
+    public static string GetStudents()
+    {
+        var response = Client.GetAsync(ApiUrl).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        return content;
+    }
+    
+    public static string GetStudent(int id)
+    {
+        var response = Client.GetAsync(ApiUrl + "/" + id).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        return content;
+    }
+    
+    public static string AddStudent(Student student)
+    {
+        var response = Client.PostAsJsonAsync(ApiUrl, student).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        return content;
+    }
+    
+    public static string UpdateStudent(int id, Student student)
+    {
+        var response = Client.PutAsJsonAsync(ApiUrl + "/" + id, student).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        return content;
+    }
+    
+    public static string DeleteStudent(int id)
+    {
+        var response = Client.DeleteAsync(ApiUrl + "/" + id).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        return content;
+    }
+}
