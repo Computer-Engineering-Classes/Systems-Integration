95705d76463e45dbe7209439cff9d7180fe8d676
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-03-21 12:56:21 +0000

    Ficha 4

---
+++ Ficha4/Queries/db_creation.sql
@@ -0,0 +1,15 @@
+﻿CREATE DATABASE Livros;
+
+USE Livros;
+GO;
+
+CREATE TABLE Livro
+(
+    Id         INTEGER IDENTITY (1, 1),
+    Titulo     NVARCHAR(MAX) NOT NULL,
+    Autor      NVARCHAR(MAX) NOT NULL,
+    Editor     NVARCHAR(MAX) NOT NULL,
+    DataCompra DATE          NOT NULL,
+    Estado     BIT           NOT NULL,
+    PRIMARY KEY (Id)
+);
