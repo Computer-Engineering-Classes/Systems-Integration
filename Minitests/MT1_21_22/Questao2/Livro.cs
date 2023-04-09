9f08155aadd17e5a0f2086c004f66ff4281c45d6
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-04-09 23:14:42 +0100

    MT1 21/22

---
+++ Minitests/MT1_21_22/Questao2/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Questao2;
+
+public class Livro
+{
+    [JsonPropertyName("ISBN")]
+    public string Isbn { get; init; } = string.Empty;
+    
+    [JsonPropertyName("titulo")]
+    public string Titulo { get; init; } = string.Empty;
+    
+    [JsonPropertyName("edicao.numero")]
+    public int EdicaoNumero { get; init; }
+    
+    [JsonPropertyName("edicao.data")]
+    public DateTime EdicaoData { get; init; }
+
+    [JsonPropertyName("edicao.exemplares")]
+    public int EdicaoExemplares { get; init; }
+    
+    [JsonPropertyName("preco_venda")]
+    public decimal PrecoVenda { get; init; }
+}
---
+++ Minitests/MT1_21_22_Especial/Questao2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Http.Json;
+using Questao2;
+
+const string apiUrl = "http://localhost/mt1";
+var client = new HttpClient();
+while (true)
+{
+    Console.WriteLine("1 - Listar Utilizadores");
+    Console.WriteLine("2 - Inserir Utilizador");
+    Console.WriteLine("3 - Alterar Estado de Utilizador");
+    Console.WriteLine("S - Sair");
+    var opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1":
+            var utilizadores = await client.GetFromJsonAsync<List<Utilizador>>($"{apiUrl}/utilizadores");
+            if (utilizadores == null) break;
+            foreach (var utilizador in utilizadores)
+            {
+                Console.WriteLine($"Id: {utilizador.Id}");
+                Console.WriteLine($"Nome: {utilizador.Nome}");
+                Console.WriteLine($"Username: {utilizador.Username}");
+                Console.WriteLine($"Password: {utilizador.Password}");
+                Console.WriteLine($"Email: {utilizador.Email}");
+                Console.WriteLine($"Estado: {utilizador.Estado}");
+                Console.WriteLine();
+            }
+
+            break;
+        case "2":
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine() ?? string.Empty;
+            Console.Write("Username: ");
+            var username = Console.ReadLine() ?? string.Empty;
+            Console.Write("Password: ");
+            var password = Console.ReadLine() ?? string.Empty;
+            Console.Write("Email: ");
+            var email = Console.ReadLine() ?? string.Empty;
+            Console.Write("Estado: ");
+            var estado = Convert.ToBoolean(Console.ReadLine());
+            var novoUtilizador = new Utilizador
+            {
+                Nome = nome,
+                Username = username,
+                Password = password,
+                Email = email,
+                Estado = estado
+            };
+            await client.PostAsJsonAsync($"{apiUrl}/utilizadores", novoUtilizador);
+            break;
+        case "3":
+            Console.Write("Id: ");
+            var id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Estado: ");
+            var novoEstado = Convert.ToBoolean(Console.ReadLine());
+            await client.PatchAsJsonAsync($"{apiUrl}/utilizadores/{id}", new { Estado = novoEstado });
+            break;
+        case "S":
+            return;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
