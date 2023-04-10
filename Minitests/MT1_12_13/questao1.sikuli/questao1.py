32791e90c81e58823f13f011f0bd4e50170f25fb
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-04-10 23:20:46 +0100

    MT1 11/12, 12/13 & 13/14

---
+++ Minitests/MT1_12_13/questao1.sikuli/questao1.py
@@ -0,0 +1,11 @@
+site = "http://files.form.guide/contact-form/simple-contact-form-1/contactform.php"
+
+type(Key.WIN)
+wait(0.1)
+type(site + Key.ENTER)
+wait(0.2)
+click("1681164590199.png")
+type(input("Your full name:") + Key.TAB)
+type(input("Your email address:") + Key.TAB)
+type(input("Message:") + Key.TAB)
+type(Key.ENTER)
