e68d10c40fd3a5f10c6d77716a53367e64d5e691
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-04-11 20:05:50 +0100

    MT1 22/23

---
+++ Minitests/MT1_22_23/Questao2/Models/Campeonato.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable IdentifierTypo, StringLiteralTypo, CommentTypo, InconsistentNaming
+
+namespace Questao2.Models;
+
+public class Campeonato
+{
+    public int Ano { get; set; }
+
+    public int JogosPorDisputar { get; set; }
+
+    public int JogosDisputados { get; set; }
+}
---
+++ Minitests/MT1_22_23/Questao2/Program.cs
@@ -0,0 +1,52 @@
+﻿// ReSharper disable IdentifierTypo, StringLiteralTypo, CommentTypo, InconsistentNaming
+
+using Questao2;
+
+while (true)
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1 - Criar campeonato");
+    Console.WriteLine("2 - Listar jogos de um campeonato");
+    Console.WriteLine("3 - Criar jogo");
+    Console.WriteLine("4 - Adicionar resultado");
+    Console.Write("Opção: ");
+    var opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1":
+            Console.Write("Ano: ");
+            var ano = Convert.ToInt32(Console.ReadLine());
+            CampeonatoManager.CreateCampeonato(ano);
+            break;
+        case "2":
+            Console.Write("Ano: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+            var jogos = CampeonatoManager.CalendarioCampeonato(ano);
+            foreach (var jogo in jogos)
+                Console.WriteLine($"Casa: {jogo.Casa} - Fora: {jogo.Fora} - Resultado: {jogo.ResultadoFinal}");
+            break;
+        case "3":
+            Console.Write("Ano: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Casa: ");
+            var casa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fora: ");
+            var fora = Convert.ToInt32(Console.ReadLine());
+            CampeonatoManager.CreateJogo(ano, casa, fora);
+            break;
+        case "4":
+            Console.Write("Ano: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Casa: ");
+            casa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fora: ");
+            fora = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Resultado: ");
+            var resultado = Console.ReadLine() ?? "Jogo não realizado";
+            CampeonatoManager.AddResultado(ano, casa, fora, resultado);
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+}
