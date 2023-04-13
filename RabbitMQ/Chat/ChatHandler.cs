4b6532f17645b85c15b41b090d304379fce01db2
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-04-13 22:58:26 +0100

    RabbitMQ -> Chat (plus other fixes)

---
+++ RabbitMQ/Chat/ChatHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Chat;
+
+public class ChatHandler
+{
+    private const string ExchangeName = "chat";
+    private readonly IModel _channel;
+    private readonly EventingBasicConsumer _consumer;
+    private readonly string _queueName;
+    private string _username = "anonymous";
+
+    public ChatHandler(IModel channel)
+    {
+        _channel = channel;
+        _channel.ExchangeDeclare(ExchangeName, ExchangeType.Fanout);
+        _queueName = _channel.QueueDeclare().QueueName;
+        _channel.QueueBind(_queueName, ExchangeName, string.Empty);
+        _consumer = new EventingBasicConsumer(_channel);
+    }
+
+    public void StartListening()
+    {
+        _consumer.Received += (_, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"\n{message}");
+        };
+        _channel.BasicConsume(_queueName, true, _consumer);
+    }
+
+    public void SendMessage(string message)
+    {
+        message = $"{_username}: {message}";
+        var body = Encoding.UTF8.GetBytes(message);
+        _channel.BasicPublish(ExchangeName, string.Empty, null, body);
+    }
+
+    public void SendJoinMessage(string username)
+    {
+        if (!string.IsNullOrWhiteSpace(username))
+            _username = username.Trim();
+        var message = $"{_username} has joined the chat";
+        var body = Encoding.UTF8.GetBytes(message);
+        _channel.BasicPublish(ExchangeName, string.Empty, null, body);
+    }
+
+    public void SendLeaveMessage()
+    {
+        var message = $"{_username} has left the chat";
+        var body = Encoding.UTF8.GetBytes(message);
+        _channel.BasicPublish(ExchangeName, string.Empty, null, body);
+    }
+}
--- RabbitMQ/Chat/Program.cs
+++ RabbitMQ/Chat/Program.cs
@@ -1,14 +1,34 @@
 ﻿using Chat;
+using RabbitMQ.Client;
 
-// Create thread for receiving messages
-var receiver = new Thread(Receiver.ReceiveMessage)
+// Create a connection to RabbitMQ server
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+// Create a channel from the connection
+using var channel = connection.CreateModel();
+// Create a chat handler
+var handler = new ChatHandler(channel);
+// Start listening for incoming messages
+handler.StartListening();
+// Ask for username
+Console.Write("Enter your username: ");
+var username = Console.ReadLine();
+if (username == null) return;
+// Send a message when the user joins the chat
+handler.SendJoinMessage(username);
+// Send a message when the user presses Ctrl+C
+// telling other users that the user has left the chat
+Console.CancelKeyPress += (_, _) =>
 {
-    IsBackground = true
+    handler.SendLeaveMessage();
+    // Wait for the message to be sent
+    Thread.Sleep(100);
 };
-
-receiver.Start();
-Sender.SendMessage();
-receiver.Join();
-
-Console.WriteLine(" Press [enter] to exit.");
-Console.ReadLine();
+// Start sending messages
+while (true)
+{
+    Console.Write("Enter message: ");
+    var message = Console.ReadLine();
+    if (message == null) continue;
+    handler.SendMessage(message);
+}
