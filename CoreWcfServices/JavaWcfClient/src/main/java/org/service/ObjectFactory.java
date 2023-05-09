33afdc5b7bb897df82347219028983cefd04264a
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-05-09 19:43:55 +0100

    Java Web Service client

---
+++ CoreWcfServices/JavaWcfClient/src/main/java/org/service/ObjectFactory.java
@@ -0,0 +1,248 @@
+
+package org.service;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the wcf.service package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: wcf.service
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Add }
+     * 
+     */
+    public Add createAdd() {
+        return new Add();
+    }
+
+    /**
+     * Create an instance of {@link MultiplyResponse }
+     * 
+     */
+    public MultiplyResponse createMultiplyResponse() {
+        return new MultiplyResponse();
+    }
+
+    /**
+     * Create an instance of {@link SubtractResponse }
+     * 
+     */
+    public SubtractResponse createSubtractResponse() {
+        return new SubtractResponse();
+    }
+
+    /**
+     * Create an instance of {@link Reverse }
+     * 
+     */
+    public Reverse createReverse() {
+        return new Reverse();
+    }
+
+    /**
+     * Create an instance of {@link Max }
+     * 
+     */
+    public Max createMax() {
+        return new Max();
+    }
+
+    /**
+     * Create an instance of {@link ArrayOfInt }
+     * 
+     */
+    public ArrayOfInt createArrayOfInt() {
+        return new ArrayOfInt();
+    }
+
+    /**
+     * Create an instance of {@link ReverseResponse }
+     * 
+     */
+    public ReverseResponse createReverseResponse() {
+        return new ReverseResponse();
+    }
+
+    /**
+     * Create an instance of {@link FahrenheitToCelsiusResponse }
+     * 
+     */
+    public FahrenheitToCelsiusResponse createFahrenheitToCelsiusResponse() {
+        return new FahrenheitToCelsiusResponse();
+    }
+
+    /**
+     * Create an instance of {@link GetIndex }
+     * 
+     */
+    public GetIndex createGetIndex() {
+        return new GetIndex();
+    }
+
+    /**
+     * Create an instance of {@link Divide }
+     * 
+     */
+    public Divide createDivide() {
+        return new Divide();
+    }
+
+    /**
+     * Create an instance of {@link Multiply }
+     * 
+     */
+    public Multiply createMultiply() {
+        return new Multiply();
+    }
+
+    /**
+     * Create an instance of {@link FahrenheitToCelsius }
+     * 
+     */
+    public FahrenheitToCelsius createFahrenheitToCelsius() {
+        return new FahrenheitToCelsius();
+    }
+
+    /**
+     * Create an instance of {@link KelvinToCelsiusResponse }
+     * 
+     */
+    public KelvinToCelsiusResponse createKelvinToCelsiusResponse() {
+        return new KelvinToCelsiusResponse();
+    }
+
+    /**
+     * Create an instance of {@link AverageResponse }
+     * 
+     */
+    public AverageResponse createAverageResponse() {
+        return new AverageResponse();
+    }
+
+    /**
+     * Create an instance of {@link CelsiusToFahrenheitResponse }
+     * 
+     */
+    public CelsiusToFahrenheitResponse createCelsiusToFahrenheitResponse() {
+        return new CelsiusToFahrenheitResponse();
+    }
+
+    /**
+     * Create an instance of {@link HelloWorldResponse }
+     * 
+     */
+    public HelloWorldResponse createHelloWorldResponse() {
+        return new HelloWorldResponse();
+    }
+
+    /**
+     * Create an instance of {@link DivideResponse }
+     * 
+     */
+    public DivideResponse createDivideResponse() {
+        return new DivideResponse();
+    }
+
+    /**
+     * Create an instance of {@link MaxResponse }
+     * 
+     */
+    public MaxResponse createMaxResponse() {
+        return new MaxResponse();
+    }
+
+    /**
+     * Create an instance of {@link Average }
+     * 
+     */
+    public Average createAverage() {
+        return new Average();
+    }
+
+    /**
+     * Create an instance of {@link AddResponse }
+     * 
+     */
+    public AddResponse createAddResponse() {
+        return new AddResponse();
+    }
+
+    /**
+     * Create an instance of {@link HelloWorld }
+     * 
+     */
+    public HelloWorld createHelloWorld() {
+        return new HelloWorld();
+    }
+
+    /**
+     * Create an instance of {@link CelsiusToFahrenheit }
+     * 
+     */
+    public CelsiusToFahrenheit createCelsiusToFahrenheit() {
+        return new CelsiusToFahrenheit();
+    }
+
+    /**
+     * Create an instance of {@link CelsiusToKelvin }
+     * 
+     */
+    public CelsiusToKelvin createCelsiusToKelvin() {
+        return new CelsiusToKelvin();
+    }
+
+    /**
+     * Create an instance of {@link CelsiusToKelvinResponse }
+     * 
+     */
+    public CelsiusToKelvinResponse createCelsiusToKelvinResponse() {
+        return new CelsiusToKelvinResponse();
+    }
+
+    /**
+     * Create an instance of {@link GetIndexResponse }
+     * 
+     */
+    public GetIndexResponse createGetIndexResponse() {
+        return new GetIndexResponse();
+    }
+
+    /**
+     * Create an instance of {@link Subtract }
+     * 
+     */
+    public Subtract createSubtract() {
+        return new Subtract();
+    }
+
+    /**
+     * Create an instance of {@link KelvinToCelsius }
+     * 
+     */
+    public KelvinToCelsius createKelvinToCelsius() {
+        return new KelvinToCelsius();
+    }
+
+}
