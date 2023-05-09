8615d198f368d4bf969d4dca7c9a408398c321e7
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-05-09 20:49:52 +0100

    Java Client (updated)

---
+++ CoreWcfServices/JavaClient/src/main/java/org/service/DivideResponse.java
@@ -0,0 +1,64 @@
+
+package org.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="DivideResult" type="{http://www.w3.org/2001/XMLSchema}float" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "divideResult"
+})
+@XmlRootElement(name = "DivideResponse")
+public class DivideResponse {
+
+    @XmlElement(name = "DivideResult")
+    protected Float divideResult;
+
+    /**
+     * Gets the value of the divideResult property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Float }
+     *     
+     */
+    public Float getDivideResult() {
+        return divideResult;
+    }
+
+    /**
+     * Sets the value of the divideResult property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Float }
+     *     
+     */
+    public void setDivideResult(Float value) {
+        this.divideResult = value;
+    }
+
+}
