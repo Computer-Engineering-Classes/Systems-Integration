0be35b37ce88221f71a6f7600816855c653cf630
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-05-25 15:16:44 +0100

    IntelliJ JDK 1.8

---
+++ WebServices/JavaClient/src/ws/Min.java
@@ -0,0 +1,65 @@
+
+package ws;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="numbers" type="{http://schemas.microsoft.com/2003/10/Serialization/Arrays}ArrayOfint" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "numbers"
+})
+@XmlRootElement(name = "Min")
+public class Min {
+
+    @XmlElementRef(name = "numbers", namespace = "http://tempuri.org/", type = JAXBElement.class, required = false)
+    protected JAXBElement<ArrayOfint> numbers;
+
+    /**
+     * Gets the value of the numbers property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link ArrayOfint }{@code >}
+     *     
+     */
+    public JAXBElement<ArrayOfint> getNumbers() {
+        return numbers;
+    }
+
+    /**
+     * Sets the value of the numbers property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link ArrayOfint }{@code >}
+     *     
+     */
+    public void setNumbers(JAXBElement<ArrayOfint> value) {
+        this.numbers = value;
+    }
+
+}
