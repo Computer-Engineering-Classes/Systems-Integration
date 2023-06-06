be987cd8af56b1bfc3a2bdd18fed07a38e7d3bef
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-06-06 13:51:49 +0100

    MT2 2018/19

---
+++ Minitests/MT2_18_19/Questao1/SensorClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// declare a fan-out exchange
+channel.ExchangeDeclare("sensor-logs", ExchangeType.Fanout);
+
+// bind the queue to the exchange
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queueName, "sensor-logs", string.Empty);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (_, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    var msgParts = message.Split(':');
+    var sensorType = msgParts[0];
+    var sensorValue = double.Parse(msgParts[1]);
+    switch (sensorType)
+    {
+        case "rpm" when sensorValue is < 1000 or > 6000:
+            Console.WriteLine($"RPM out of range: {sensorValue}");
+            break;
+        case "l/km" when sensorValue > 40:
+            Console.WriteLine($"Fuel consumption out of range: {sensorValue}");
+            break;
+    }
+};
+
+channel.BasicConsume(queueName, true, consumer);
+
+while (true)
+{
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
+    break;
+}
---
+++ Minitests/MT2_18_19/Questao1/SensorMQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("sensor-logs", ExchangeType.Fanout);
+
+while (true)
+{
+    Console.WriteLine("Sensor type:");
+    Console.WriteLine("1 - Rotation speed (revolutions per minute)");
+    Console.WriteLine("2 - Fuel consumption (liters per kilometer)");
+    Console.WriteLine("Option:");
+    if (!int.TryParse(Console.ReadLine(), out var option)) continue;
+    Console.WriteLine("Value:");
+    if (!double.TryParse(Console.ReadLine(), out var value)) continue;
+    var message = option switch
+    {
+        1 => $"rpm:{value}",
+        2 => $"l/km:{value}",
+        _ => null
+    };
+    if (message == null) continue;
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish("sensor-logs",
+        string.Empty,
+        null,
+        body);
+    Console.WriteLine($"Sent: {message}");
+}
