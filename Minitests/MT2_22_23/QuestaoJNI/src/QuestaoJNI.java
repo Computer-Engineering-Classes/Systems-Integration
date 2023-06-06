cee61507a034e0023bc326e990f789f468d0e876
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-06-07 00:02:00 +0100

    Quick fixes
    Test template:
    - proper RabbitMQ subscriber
    - JNI now takes in input from user

--- Minitests/MT2_22_23/QuestaoJNI/src/QuestaoJNI.java
+++ Minitests/MT2_22_23/QuestaoJNI/src/QuestaoJNI.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 public class QuestaoJNI {
     static {
         System.loadLibrary("Questao");
     }
 
     public native int add(int a, int b);
+
     public native int subtract(int a, int b);
 
     public static void main(String[] args) {
         QuestaoJNI jni = new QuestaoJNI();
-        System.out.println("Soma: " + jni.add(10, 20));
-        System.out.println("Subtração: " + jni.subtract(10, 20));
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter two numbers: ");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Sum: " + jni.add(a, b));
+            System.out.println("Subtraction: " + jni.subtract(a, b));
+        }
     }
 }
