68f662299a53980a0e183783b1bf6468d925cb60
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-06-06 21:54:20 +0100

    MT2 2018/19 (2 & 3)

---
+++ Minitests/MT2_18_19/Questao3/WcfService/IStringOperator.cs
@@ -0,0 +1,17 @@
+﻿namespace WcfService;
+
+[ServiceContract]
+public interface IStringOperator
+{
+    [OperationContract]
+    int CountZeros(string str);
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class StringOperator : IStringOperator
+{
+    public int CountZeros(string str)
+    {
+        return str.Count(c => c == '0');
+    }
+}
